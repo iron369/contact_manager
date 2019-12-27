bb810638e43036e1b94bf5a6199a34084a744fdd
iron369 <58945299+iron369@users.noreply.github.com>
2019-12-27 13:02:00 +0800

    Add files via upload

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -10,26 +10,28 @@ const Header = props => {
         <a href="/" className="navbar-brand">
           {branding}
         </a>
+        <div>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                <i className="fas fa-home"></i> Home
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link to="/contact/add" className="nav-link">
+                <i className="fas fa-plus"></i> Add Contact
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link to="/about" className="nav-link">
+                <i className="fas fa-question"></i> About
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
-      <ul className="navbar-nav mr-auto">
-        <li className="nav-item">
-          <Link to="/" className="nav-link">
-            <i className="fas fa-home"></i> Home
-          </Link>
-        </li>
-
-        <li className="nav-item">
-          <Link to="/contact/add" className="nav-link">
-            <i className="fas fa-plus"></i> Add Contact
-          </Link>
-        </li>
-
-        <li className="nav-item">
-          <Link to="/about" className="nav-link">
-            <i className="fas fa-question"></i> About
-          </Link>
-        </li>
-      </ul>
     </nav>
   );
 };
